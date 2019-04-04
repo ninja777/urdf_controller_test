a4aec7161bf74dc8f766fbe96a4227873514eb5b
Vinayak Jagtap <vinayak.jagtap04@gmail.com>
2019-04-04 11:47:36 -0400

    sanitized the ros package

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/dnguyen2/personal-project/urdf_controller_test/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/dnguyen2/rqt_test/devel;/home/dnguyen2/mqp_ws/devel;/home/dnguyen2/urdf_test2_project/devel;/opt/ros/kinetic".split(';') if "/home/dnguyen2/rqt_test/devel;/home/dnguyen2/mqp_ws/devel;/home/dnguyen2/urdf_test2_project/devel;/opt/ros/kinetic" != "" else []
--- build/urdf_display/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dnguyen2/personal-project/urdf_controller_test/src/urdf_display
-# Build directory: /home/dnguyen2/personal-project/urdf_controller_test/build/urdf_display
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
